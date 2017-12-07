40dd51527ba4095673f8048d5e72c9bdca1c72f7
ralucamiclea <rmiclea02@gmail.com>
2017-12-07 20:12:11 +0200

    added search through old conversations

--- app/src/main/java/com/dirtybits/privatechat/Chat.java
+++ app/src/main/java/com/dirtybits/privatechat/Chat.java
@@ -1,43 +1,80 @@
 package com.dirtybits.privatechat;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Chat extends AppCompatActivity {
 
+    FloatingActionButton fab;
     ListView chatList;
-    List list = new ArrayList();
+    List<String> list;
     ArrayAdapter adapter;
+    EditText filterText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        chatList = (ListView) findViewById(R.id.chatList);
 
-        //TODO: get old user conversations from the server
-        list.add("user1");
-        list.add("user2");
-        list.add("user3");
-        list.add("user4");
-        list.add("user5");
-        list.add("user6");
-        list.add("user7");
-        list.add("user8");
-        list.add("user9");
-        list.add("user10");
-        list.add("user11");
-        list.add("user12");
-        list.add("user13");
-        list.add("user14");
-        list.add("user15");
+        filterText = (EditText) findViewById(R.id.search_old_conv);
+        chatList = (ListView) findViewById(R.id.chatList);
+        fab = (FloatingActionButton) findViewById(R.id.fab_write_msg);
+
+        //TODO: get usernames from the server
+        String[] values = {"user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9", "user10", "user11", "user12", "user13"};
+        list = new ArrayList<String>();
+        Collections.addAll(list,values);
 
         adapter = new ArrayAdapter(Chat.this, android.R.layout.simple_list_item_1, list);
         chatList.setAdapter(adapter);
+
+        chatList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // make Toast when click
+                Toast.makeText(getApplicationContext(), "Position " + position + 1, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        filterText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Chat.this.adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        //go to NewConv Activity
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Chat.this, NewConv.class));
+            }
+        });
     }
 }
